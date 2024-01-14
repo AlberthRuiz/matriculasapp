013f3a45872485337735fa0c66a51f7fce5034d0
Christiam Mendoza <ch.alb.rz@hotmail.com>
2024-01-14 11:02:23 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:matriculasapp/screens/matricula_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MatriculasHome(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
---
+++ lib/screens/matricula_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:matriculasapp/models/Person_model.dart';
+
+class MatriculasHome extends StatefulWidget {
+  @override
+  State<MatriculasHome> createState() => _MatriculasHomeState();
+}
+
+class _MatriculasHomeState extends State<MatriculasHome> {
+  List<Person> peopleList = [
+    Person(name: "Ana", address: "av 1245", phone: "12345679"),
+    Person(name: "Lia", address: "av lima", phone: "9751665"),
+    Person(name: "Maria", address: "av mlsis", phone: "88888888"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              peopleList.add(
+                Person(
+                    name: "Pedro",
+                    address: "av Larc0 789",
+                    phone: "1938771",
+                    dni: "12345678"),
+              );
+              setState(() {});
+            },
+            child: Text("Agregar"),
+          ),
+          ...peopleList.map((e) => ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.cyan,
+                  radius: 25,
+                  child: Text(
+                    e.name[0],
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                title: Text(e.name),
+                subtitle: Text(e.address),
+                trailing: Text(e.phone),
+              )),
+        ],
+      ),
+    );
+  }
+}
